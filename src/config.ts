3b25eb5863d6f9070ea2c3243864114ce7086040
Cristóbal Jurado Oller <c.jurado.oller@gmail.com>
2024-02-04 23:30:05 +0100

    Initialize

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Porfolio de Cristóbal Jurado -  Desarrollador Web";
+export const SITE_DESCRIPTION =
+  "Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
